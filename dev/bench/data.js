998ea446ad851e16dc15e477f63db3f9f692a87a
github-action-benchmark <github@users.noreply.github.com>
2025-12-16 12:13:31 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 763cb483f4c8e605b3182c8b1d076cdd920739b2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765887210174,
+  "lastUpdate": 1765887211891,
   "repoUrl": "https://github.com/defranchis/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -555,6 +555,75 @@ window.BENCHMARK_DATA = {
             "range": "10",
             "unit": "Seconds",
             "extra": "Analysis path: examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "763cb483f4c8e605b3182c8b1d076cdd920739b2",
+          "message": "Allowing a timestamp in output-dir-eos (#492)\n\n* Allowing a timestamp in output-dir-eos\n\n* Do not check if EOS output directory exists when running analysis",
+          "timestamp": "2025-12-10T11:06:38+01:00",
+          "tree_id": "0e877f0ddfe45dae3108dde30f3629034a57cbc1",
+          "url": "https://github.com/defranchis/FCCAnalyses/commit/763cb483f4c8e605b3182c8b1d076cdd920739b2"
+        },
+        "date": 1765887211224,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.575614890302138,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.41526047637034,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/ee/analysis_stage1.py",
+            "value": 8.31987435638961,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 4.698722950763384,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.786207985556302,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.0863633006224993,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/stage1.py",
+            "value": 3.2830648058199308,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/stages_source.py",
+            "value": 4.213834109778638,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/analysis_stage1.py",
+            "value": 4.1167094203163765,
+            "unit": "Evt/s"
           }
         ]
       }
