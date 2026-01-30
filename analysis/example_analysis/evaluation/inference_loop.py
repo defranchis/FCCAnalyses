309bef233c2841292c9d04bf93f200b13baf3b39
LukaLambrecht <Luka.Lambrecht@UGent.be>
2026-01-30 14:45:33 +0100

    checkpoint before trying to pull upstream master back into this fork/branch

--- analysis/example_analysis/evaluation/inference_loop.py
+++ analysis/example_analysis/evaluation/inference_loop.py
@@ -13,8 +13,8 @@ import tools.slurmtools as st
 if __name__=='__main__':
 
     # settings
-    modeltag = '20260122_withsv'
-    ntupletag = 'withsv'
+    modeltag = '20260123_withsv2perevent'
+    ntupletag = 'withsv2perevent'
     model = os.path.abspath(f'models/output_{modeltag}/model.onnx')
     preprocess = model.replace('model.onnx', 'preprocess.json')
     outputdir = f'output_scores_model_{modeltag}'
