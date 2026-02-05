1a37882eb5cf03f1063131f71079aef8179b5709
LukaLambrecht <Luka.Lambrecht@UGent.be>
2026-02-05 13:35:48 +0100

    small tools update

--- analysis/utils/print_nevents.py
+++ analysis/utils/print_nevents.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import ROOT
+import numpy as np
 
 
 if __name__=='__main__':
@@ -28,4 +29,15 @@ if __name__=='__main__':
     print('Found following number of events:')
     for key, val in nevents.items():
         print(f'  - {key}: {val}')
-    print(f'  --> total: {sum(list(nevents.values()))}')
+    print('Summary statistics:')
+    ns = np.array(list(nevents.values()))
+    ntot = np.sum(ns)
+    navg = np.mean(ns)
+    nstd = np.std(ns)
+    nmax = np.amax(ns)
+    nmin = np.amin(ns)
+    print(f' - Total: {ntot}')
+    print(f' - Average: {navg}')
+    print(f' - Std: {nstd}')
+    print(f' - Max: {nmax}')
+    print(f' - Min: {nmin}')
