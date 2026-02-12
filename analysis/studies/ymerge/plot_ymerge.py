7de6b3bd75c1ae46ef2d6403f7a4182a9e41e9f3
LukaLambrecht <Luka.Lambrecht@UGent.be>
2026-02-12 13:25:33 +0100

    add plotting script for y23 variable

---
+++ analysis/studies/ymerge/plot_ymerge.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import uproot
+import awkward as ak
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+if __name__=='__main__':
+
+    # settings
+    inputfiles = sys.argv[1:]
+    treename = 'events'
+    outputdir = 'output_plots'
+
+    variables = [
+      'Event_dmerge2',
+      'Event_dmerge3',
+      'Event_dmerge4',
+    ]
+
+    # make output dir if needed
+    if not os.path.exists(outputdir): os.makedirs(outputdir)
+
+    # read input files
+    batches = []
+    branches_to_read = variables
+    for idx, inputfile in enumerate(inputfiles):
+        print(f'Reading file {idx+1} / {len(inputfiles)}', end='\r')
+        readstr = ':'.join([inputfile, treename])
+        with uproot.open(readstr) as f:
+            batches.append(f.arrays(branches_to_read))
+    events = ak.concatenate(batches)
+    print(f'Read {len(events)} entries.')
+
+    # make kinematic mask (optional)
+    kinematic_mask = np.ones(len(events)).astype(bool)
+    print(f'Made kinematic mask with {np.sum(kinematic_mask)} / {len(kinematic_mask)} entries passing.')
+
+    # loop over dmerge variables
+    data = {}
+    for variable in variables:
+    
+            # get data
+            values = events[variable][kinematic_mask]
+            
+            # parsing
+            values = values.to_numpy()
+            if np.isnan(values).any():
+                msg = 'WARNING: replacing NaN by 0...'
+                print(msg)
+                np.nan_to_num(values, copy=False, nan=0)
+
+            # add to struct
+            data[variable] = values
+
+            # make y_merge from d_merge
+            y_merge = values / 91.2**2
+
+            data[variable.replace('dmerge', 'ymerge')] = y_merge
+
+    # determine ymerge cutoff where 3% of events are removed
+    variable = 'Event_ymerge2'
+    cutoff_amount = 0.03
+    values = data[variable]
+    cutoff_value = np.quantile(values, 1-cutoff_amount)
+    print(f'Cutoff value where {cutoff_amount*100}% of events are removed (for variable {variable}): {cutoff_value}')
+
+    # loop over types
+    for vartype in ['dmerge', 'ymerge']:
+
+        # set bins
+        bins = np.linspace(0, 0.4, num=51)
+        if vartype == 'dmerge': bins = np.linspace(0, 500, num=51)
+
+        # make figure
+        fig, ax = plt.subplots()
+        for variable, values in data.items():
+            if vartype not in variable: continue
+            hist = np.histogram(values, bins=bins, density=True)[0]
+            label = variable.replace('Event_', '')
+            ax.stairs(hist, edges=bins, label=label, linewidth=2)
+
+        # add cutoff
+        if vartype=='ymerge':
+            ax.axvline(x=cutoff_value, linestyle='--', color='r', label=f'{cutoff_amount*100}% cutoff')
+
+        # plot aesthetics
+        ax.set_ylabel('Events (normalized)', fontsize=12)
+        ax.set_xlabel(vartype, fontsize=12)
+        ax.grid(which='both', axis='both')
+        ax.legend(fontsize=12)
+        ax.set_yscale('log')
+
+        # save figure
+        fig.tight_layout()
+        outputfile = os.path.join(outputdir, f'{vartype}.png')
+        fig.savefig(outputfile)
+        plt.close()
